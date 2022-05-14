21f4d47904a708963634056fac1b817d9d4b212d
Paulo J. Cagol <paulojosecagol@gmail.com>
2022-05-14 11:50:18 -0300

    Service layer, component registration Order, Instant, ISO 8601

--- src/main/java/com/dev/paulocagol/course/resources/UserResource.java
+++ src/main/java/com/dev/paulocagol/course/resources/UserResource.java
@@ -1,20 +1,35 @@
 package com.dev.paulocagol.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dev.paulocagol.course.entities.User;
+import com.dev.paulocagol.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@maria.com", "999999", "123");
+	@Autowired
+	private UserService service;
 
-		return ResponseEntity.ok().body(u);
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+
+		return ResponseEntity.ok().body(obj);
 	}
 }
